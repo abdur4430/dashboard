4b6442632936d71aecc247dc7fea24eeb1e1219a
Abdur Rehman <abdus4430@gmail.com>
2025-07-02 15:04:45 +0500

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,10 @@ const HeaderComponent = () => (
         defaultSelectedKeys={['1']}
         style={{ lineHeight: '52px' }}
       >
-        <Menu.Item key='1'><Link to='/'>Desafio_1</Link></Menu.Item>
+        <Menu.Item key='1'><Link to='/'>Abdur Rehman</Link></Menu.Item>
         <Menu.Item key='2'><Link to='/about'>About</Link></Menu.Item>
         <Menu.Item key='3'>
-          <a href='https://github.com/nickbouldien/dashboard_desafio' target='_blank' rel='noreferrer noopener'>
+          <a href='https://github.com/abdur4430' target='_blank' rel='noreferrer noopener'>
             <Icon type='github' style={{ fontSize: 24, color: '#08c', marginTop: '15px' }} />
           </a>
         </Menu.Item>
